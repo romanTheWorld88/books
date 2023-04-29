8aadb124a9144be6718c71115d17a362c88e9466
RyanRoman <C00412434@louisiana.edu>
2023-04-29 12:44:59 -0500

    abc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   // we are going to be updating state that is an array
   const [books, setBooks] = useState([]);
 
-
   // use map to update an object in an array
   // if id is equal to id passed, it is the one we want to update
-  // so we update by returning a new object that takes existing properties from 
-  // book and puts new title in 
+  // so we update by returning a new object that takes existing properties from
+  // book and puts new title in
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
-      if (book.id === id){
-        return { ...book, title: newTitle};
+      if (book.id === id) {
+        return { ...book, title: newTitle };
       }
 
       return book;
-
     });
     setBooks(updatedBooks);
   };
@@ -33,17 +32,23 @@ function App() {
   };
 
   // handle submission of title
-  const createBook = (title) => {
+  const createBook = async (title) => {
+    const response = await axios.post("https://localhost:3001/books", {
+      title,
+    });
+
+    /*
     const updatedBooks = [
       ...books,
       { id: Math.round(Math.random() * 999), title },
     ];
     setBooks(updatedBooks);
+    */
   };
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList onEdit = {editBookById} books={books} onDelete={deleteBookById} />
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
